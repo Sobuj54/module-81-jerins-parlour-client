498e699fdca929d72b454eadacd1da046f534985
soubj <sobujahmed124@gmail.com>
2023-09-05 22:17:41 +0600

    created a footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Footer from "../../components/Footer/Footer";
 import HeroSection from "../../components/HeroSection/HeroSection";
 
 const Home = () => {
   return (
     <>
       <HeroSection></HeroSection>
+      <Footer></Footer>
     </>
   );
 };
