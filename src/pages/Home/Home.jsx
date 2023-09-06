33045a26872ca60b945c4f02967ca2e7e1f106e0
soubj <sobujahmed124@gmail.com>
2023-09-06 22:20:09 +0600

    created fake service data and added service section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import Contact from "../../components/Contact/Contact";
 import Footer from "../../components/Footer/Footer";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import MidHero from "../../components/MidHero/MidHero";
+import SampleServices from "../../components/Services/SampleServices";
 
 const Home = () => {
   return (
     <>
       <HeroSection></HeroSection>
+      <SampleServices></SampleServices>
       <MidHero></MidHero>
       <Contact></Contact>
       <Footer></Footer>
